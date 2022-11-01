db696de22062bac70b25a0caff85eb637dd7a263
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-01 16:44:05 +0900

    add validate

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -3,3 +3,34 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private readonly config: ConfigService,
+    private readonly prisma: PrismaService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req) => {
+          let jwt = null;
+          if (req && req.cookies) {
+            jwt = req.cookies['access_token'];
+          }
+          return jwt;
+        },
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+  async validate(palyload: { sub: number; email: string }) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: palyload.sub,
+      },
+    });
+    delete user.hashedPassword;
+    return user;
+  }
+}
