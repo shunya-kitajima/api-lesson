21489a350143a65d6e2e5a712f1f949cc32a5e91
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-01 15:38:35 +0900

    import modules

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateUserDto } from './dto/Update-user.dto';
 
 @Injectable()
 export class UserService {}
