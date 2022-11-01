ca85c2e5a1c2c9429b3ee70787fef73002541950
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-01 16:13:04 +0900

    import modules

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,5 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
