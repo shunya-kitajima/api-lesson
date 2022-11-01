c26477167863b5d5858d5b27b003f5b896794b55
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-01 11:35:17 +0900

    creat methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,4 +8,56 @@ import { AuthDto } from './dto/auth.dto';
 import { Msg, Jwt } from './interfaces/auth.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly config: ConfigService,
+    private readonly jwt: JwtService,
+    private readonly prisma: PrismaService,
+  ) {}
+  signup = async (dto: AuthDto): Promise<Msg> => {
+    const hashed = await bcrypt.hash(dto.password, 12);
+    try {
+      await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hasedPassword: hashed,
+        },
+      });
+      return {
+        message: 'ok',
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('This email is already taken');
+        }
+      }
+      throw error;
+    }
+  };
+  generateJwt = async (userId: number, email: string): Promise<Jwt> => {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '5m',
+      secret: secret,
+    });
+    return {
+      accessToken: token,
+    };
+  };
+  login = async (dto: AuthDto): Promise<Jwt> => {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) throw new ForbiddenException('Email or password incorrect');
+    const isValid = await bcrypt.compare(dto.password, user.hasedPassword);
+    if (!isValid) throw new ForbiddenException('Email or password incorrect');
+    return this.generateJwt(user.id, user.email);
+  };
+}
